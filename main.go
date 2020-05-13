4584383f7998206d6c8683c0811bdbc1823d5125
Denis Onder <denis_onder@protonmail.com>
2020-05-13 04:52:26 +0200

    Established WebSocket connection between client and server

--- main.go
+++ main.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 // PORT => Server port
 const PORT = ":5000"
 
-func serveHomePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func serveWSEndpoint(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("serveWSEndpoint", err)
+		return
+	}
+	conn.WriteMessage(1, []byte("Welcome"))
 }
 
 func main() {
-	// Handlers
-	http.HandleFunc("/", serveHomePage)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", serveWSEndpoint)
 	// Start the server
 	fmt.Printf("Server running!\nhttp://localhost%s/\n", PORT)
 	err := http.ListenAndServe(PORT, nil)
